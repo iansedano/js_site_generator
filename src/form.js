49a302dc8b9752e0b82dfafdef7359d4e58ff522
Ian <iansedano@gmail.com>
2021-12-06 15:26:14 +0100

    simple email validation

--- src/form.js
+++ src/form.js
@@ -1,3 +1,17 @@
+function emailValidator(e) {
+	if (!isEmail(e.target.value)){
+		e.target.style.backgroundColor = "red"
+	} else e.target.style.backgroundColor = ""
+}
+
+function isEmail(string) {
+	const emailRegex = /.+@.+/
+	
+	if (string.match(emailRegex)) {
+		return true
+	} else return false
+}
+
 function build_form(fields) {
 	console.log(fields)
 	const form_element = c("form")
@@ -23,11 +37,13 @@ function build_form(fields) {
 			const input = c("input")
 			input.type = "text"
 			input.id = "input" + field.type + field.label
+			input.addEventListener("input", emailValidator)
 			
 			const br = c("br")
 			
 			const label = c("label", field.label)
 			label.htmlFor = "input" + field.type + field.label
+			
 			
 			fieldContainer.append(label, br, input)
 			form_element.append(fieldContainer)

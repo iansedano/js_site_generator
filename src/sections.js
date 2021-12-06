a433baee9a5b4131ec9c76e8398de34853b7a1bc
Ian <iansedano@gmail.com>
2021-12-06 15:02:11 +0100

    first form element

--- src/sections.js
+++ src/sections.js
@@ -73,7 +73,7 @@ function contact(section) {
 	}
 	
 	if (section.hasOwnProperty("form")) {
-		container.append(form(container.form))
+		container.append(form(section.form))
 	}
 	
 	renderImage(section, container)
@@ -82,7 +82,28 @@ function contact(section) {
 }
 
 function form(fields) {
-	return ""
+	console.log(fields)
+	const form_element = c("form")
+	
+	fields.forEach((field) => {
+		if (field.type == "text") {
+			const fieldContainer = c("p")
+			
+			const input = c("input")
+			input.type = "text"
+			input.id = "input" + field.type + field.label
+			
+			const br = c("br")
+			
+			const label = c("label", field.label)
+			label.htmlFor = "input" + field.type + field.label
+			
+			fieldContainer.append(label, br, input)
+			form_element.append(fieldContainer)
+		}
+	})
+	
+	return form_element
 }
 
 function links(section) {
